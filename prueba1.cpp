1bfbbb8c64444d94a6c020bb379d2815f1cd5f15
Jose Alburez <jalburez@miugm.edu.gt>
2024-02-26 20:02:14 -0600

    tercer ejercicio

---
+++ prueba1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*std::cout << "Hello, World!" << std::endl;*/ //primer ejercicio
+/*double numero;
+cout<<"ingresar un numer"; // segundo ejercicio
+cin>>numero;
+if(numero >0){
+    cout<<"numero es positivo"<<endl;
+
+} else if(numero <0){
+        cout<<"numero es negativo"<<endl;
+
+}else{
+    cout<<"El numero es cero"<<endl;*/
+    int numero;
+    cout<<"ingresa un numero entero"<<endl;
+    cin>> numero;
+    if (numero % 2 == 0){
+        cout<<"El numero ingresado es par"<<endl;
+    }else{
+        cout<<"El numero ingresado es impar"<<endl;
+    }
+
+
+
+
+
+
+     return 0;
+}
